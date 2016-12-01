54df5879a25558992213157e97a5f2d4ba0de0d1
Mashrur Mia (Sa'ad) <m.mia@psenterprise.com>
2016-12-01 16:05:35 +0000

    new url, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -39,21 +39,20 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         # when she hits enter, she is taken to a new URL,
-        # and now the page list "1: Buy peacock feathers" as an ietm in a
+        # and now the page list "1: Buy peacock feathers" as an item in a
         # to-do list table
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        
+
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
