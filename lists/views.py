29d7bc2ad93b9163aff248f3f2a937ffd5b08bd8
Mashrur Mia (Sa'ad) <m.mia@psenterprise.com>
2016-12-01 16:31:15 +0000

    add URL for creating new list via POST

--- lists/views.py
+++ lists/views.py
@@ -4,12 +4,11 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    if(request.method == 'POST'):
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-    else:
-        return render(request, 'home.html')
-    pass
+    return render(request, 'home.html')
 
 def view_list(request):
     return render(request, 'list.html', {'items':Item.objects.all})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
