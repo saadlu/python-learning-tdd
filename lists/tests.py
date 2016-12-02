119fb9518ef17f6bb45b1f2e26acd9db756ea63e
Mashrur Mia (Sa'ad) <m.mia@psenterprise.com>
2016-12-02 19:20:22 +0000

    new URL + view for adding to existing lists. FT passes.

--- lists/tests.py
+++ lists/tests.py
@@ -60,6 +60,13 @@ class ListViewTest(TestCase):
 
         self.assertTemplateUsed(response, 'list.html')
 
+    def test_pass_correct_list_to_template(self):
+        correct_list = List.objects.create()
+        other_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id))
+
+        self.assertEqual(response.context['list'], correct_list)
+
     def test_display_all_items(self):
         correct_list = List.objects.create()
         Item.objects.create(text='itemey 1', list=correct_list)
@@ -76,6 +83,29 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 2')
 
 class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post('/lists/%d/add_item' % (correct_list.id),
+                         data={'item_text':'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post('/lists/%d/add_item' % (correct_list.id),
+                         data={'item_text':'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id))
 
     def test_saving_POST_request(self):
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
